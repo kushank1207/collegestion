ac237376030fd73e79251189d8f96f2a6b1ca544
SHIVANSH <43453205+LouisPaul2000@users.noreply.github.com>
2022-04-01 04:55:37 -0400

    Client updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-import Product from './components/product'
+import Home from './components/Home'
+import Login from "./components/Login";
+import { Routes, Route, Link } from "react-router-dom";
+import Product from "./components/Product";
+import Reports from './components/Reports';
 
 function App() {
   return (
-    <Product />
+      <div className="App">
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/reports" element={<Reports />} />
+        </Routes>
+      </div>
   );
 }
 
