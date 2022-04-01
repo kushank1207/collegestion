ce312dd870c61399970902d485c1ee71b1e6b92c
SHIVANSH <43453205+LouisPaul2000@users.noreply.github.com>
2022-04-01 14:16:52 +0530

    Update order.go

--- server/models/order.go
+++ server/models/order.go
@@ -5,9 +5,12 @@ import (
 )
 
 type Product struct {
-	Prod_ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name    string             `json:"name,omitempty" bson:"name,omitempty"`
-	Price   float64              `json:"price,omitempty" bson:"price,omitempty"`
+	Prod_ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Price       string             `json:"price,omitempty" bson:"price,omitempty"`
+	Description string             `json:"description" bson:"description"`
+	Country     string             `json:"country" bson:"country"`
+	itemcode    string             `json:"itemcode" bson:"itemcode"`
 }
 
 type Customer struct {
