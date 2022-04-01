a5ab31bd6085109fda232804c91e67133fe2baf8
Kumar Vinayak <kvinayak6@yahoo.co.in>
2022-04-01 01:54:18 -0400

    Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Product from './components/product'
-
+import Product from './components/product.js'
+import Dashboard from './components/Dashboard.js'
 function App() {
   return (
     <Product />
