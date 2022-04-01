af60be988ab44dbc6c22d221fdb0222c55b00ded
Kushank <singh.kushank12@gmail.com>
2022-04-01 18:03:24 -0400

    Added Sign up page and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+<<<<<<< Updated upstream
 import Login from './components/Login.js'
 
 function App() {
   return (
     <Login />
+=======
+import ReactDOM from "react-dom";
+import Product from './components/Product.js'
+import Dashboard from './components/Dashboard.js'
+import Login from './components/Login.js'
+import SignUp from './components/SignUp.js'
+import NoPage from './components/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="Product" element={<Product />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter> 
+>>>>>>> Stashed changes
   );
 }
 
