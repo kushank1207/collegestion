0296c8d57024bd7ebaf55eaa3d55a2eee890edab
SHIVANSH <43453205+LouisPaul2000@users.noreply.github.com>
2022-04-08 07:02:55 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,8 @@
-@@ -2,8 +2,1777 @@ import React from 'react';
-import ReactDOM from 'react-dom';	import ReactDOM from 'react-dom';
-import './index.css';	import './index.css';
-import App from './App';	import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
-import SnackbarProvider from 'react-simple-snackbar'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-
-ReactDOM.render(	ReactDOM.render(
-    <App />,	    <BrowserRouter>
-    <SnackbarProvider>
-    <App />
-    </SnackbarProvider>
-
-    </BrowserRouter>,
-  document.getElementById('root')	  document.getElementById('root')
-); 
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
