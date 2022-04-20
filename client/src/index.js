cd8e1fe3355a22e9bdb473309593d67bf685f095
SHIVANSH <43453205+LouisPaul2000@users.noreply.github.com>
2022-04-20 08:09:36 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,22 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import { ThemeProvider } from './components/ThemeContext';
+import Background from './components/Background';
+import Toggle from './components/ThemeToggle';
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+    <SnackbarProvider>
+    <ThemeProvider>
+      <Background>
+         <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+            <Toggle />
+          </div>
+
+        <App />
+          </Background>
+    </ThemeProvider>,
+    </SnackbarProvider>
+
+    </BrowserRouter>,
